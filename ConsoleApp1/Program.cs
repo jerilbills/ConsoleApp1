a929aad0955a212492efcffaa3c6f8e230cbdf29
Ifeyinwa Uwazie <icu2065@gmail.com>
2024-02-13 15:13:19 -0500

    UI class + edits

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,41 +10,8 @@ public class Program
 
     public static void Main(string[] args)
     {
-        Dictionary<string, string> moves = new Dictionary<string, string>()
-        {
-            {"r", "rock" },
-            {"p", "paper" },
-            {"s", "scissors" }
-        };
-    // Give instructions to user
-    Console.WriteLine("Welcome to Rock Paper Scissors.");
-        Console.WriteLine("I will be your opponent. The instructions are as follows:");
-        Console.WriteLine("You will then be prompted to select your move- R=rock, P= paper & S=scissors");
-        Console.WriteLine("Rock smashes scissors. Paper covers rock. Scissors cut paper.");
-        Console.WriteLine("Matching moves result in a tie.");
-        //code for rock->paper->scissors count down
-        Thread.Sleep(18000);
-        
-
-        Console.WriteLine($"Rock!");
-        Thread.Sleep(2000); //Wait for 1 second
-        Console.Clear(); //Clear the console for a countdown effect
-        Console.WriteLine($"Paper!");
-        Thread.Sleep(2000);
-        Console.Clear();
-        Console.WriteLine($"Scissors!");
-        Thread.Sleep(2000);
-        Console.Clear();
-
-        Console.WriteLine("Select your move: R, P, or S");
-        string userInput;
-        userInput = Console.ReadLine();
-        string userInputCaseInsensitive = userInput.ToLower();
-        string userMove = moves[userInputCaseInsensitive];
-        //method HERE for CPU's random move
-        ComputerPlayer cpu = new ComputerPlayer();
-        string cpumove = cpu.CpuMove();
-        Console.WriteLine($"Opponent chose {cpumove}");
+        UI uI = new UI();
+        uI.Run();
     }
 }
 
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a95975e51114bea16d714985927032580ed5c03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f99dd1f078d261c4fa3b62a4cd296c0831c9b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
